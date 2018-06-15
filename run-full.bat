f68d07f6c2389962af0faba60a7c65dfd8f7f1d5
Frank Niessink <frank@niessink.com>
2018-06-15 18:43:24 +0200

    Remove unused code.

--- run-full.bat
+++
@@ -1,3 +0,0 @@
-call node_modules\.bin\markdown-include.cmd .\DocumentDefinitions\Full\document.json
-call node_modules\.bin\markdown.cmd ICTU-Kwaliteitsaanpak-Full.md -s %cd%/DocumentDefinitions/Full/document.css > ICTU-Kwaliteitsaanpak-Full.html
-call node htmltopdf.js ICTU-Kwaliteitsaanpak-Full.html

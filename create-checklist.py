cd7ae45bb2ef89717ec79a46de9f60921d05888c
Frank Niessink <frank@niessink.com>
2018-07-23 20:15:59 +0200

    Add document structure to checklist.

--- create-checklist.py
+++ create-checklist.py
@@ -89,11 +89,25 @@ def write_assessment_choices(workbook, worksheet, start_row, end_row, column):
         dict(validate="list", source=[choice[0] for choice in assessment_choices]))
 
 
-def maatregelen_folders():
-    """ Return a list of maatregelen folders in the right order. """
-    with pathlib.Path("DocumentDefinitions/Full/document.md").open() as document_structure:
-        for line in document_structure.readlines():
-            if '/Maatregel.md"' in line:
+class DocumentStructure:
+    """ Class representing the document structure of the kwaliteitsaanpak. """
+    def __init__(self):
+        with pathlib.Path("DocumentDefinitions/Full/document.md").open() as document_structure:
+            self.lines = document_structure.readlines()
+
+    def maatregelen_sections(self):
+        """ Return a list of sections in the document. """
+        for line in self.lines:
+            if line.startswith("##") and not "Bijlagen" in line:
+                yield line.strip().strip("## ")
+
+    def maatregelen_folders(self, section):
+        """ Return a list of maatregelen folders in the right order. """
+        in_section = False
+        for line in self.lines:
+            if line.startswith("## "):
+                in_section = section in line
+            if in_section and '/Maatregel.md"' in line:
                 yield pathlib.Path(line.strip().strip('# include "').strip('/Maatregel.md"'))
 
 
@@ -126,8 +140,12 @@ def create_checklist():
 
     maatregel_start_row = row = 2
 
-    for maatregel_folder in maatregelen_folders():
-        row = process_maatregel(workbook, worksheet, maatregel_folder, row)
+    document = DocumentStructure()
+    for section in document.maatregelen_sections():
+        row += 1
+        worksheet.merge_range(f"A{row}:D{row}", section, header_format)
+        for folder in document.maatregelen_folders(section):
+            row = process_maatregel(workbook, worksheet, folder, row)
 
     status_column = 2
     write_assessment_choices(workbook, worksheet, maatregel_start_row, row, status_column)

7037befd02da32de89a31e374caabeeafe5880de
Barleduc <lepelrekje@xs4all.nl>
2019-08-20 14:35:47 +0200

    shared cover definition

---
+++ DocumentDefinitions/Templates/Shared/cover.md
@@ -0,0 +1,7 @@
+![ICTU](../../../Content/Images/ICTU.png "ictu-logo")
+
+# {{TITLE}}
+### {Projectnaam}
+Versie {versienummer}
+
+![wordcloud](../../../Content/Images/word-cloud.png "word-cloud")
--- create-docs.sh
+++ create-docs.sh
@@ -3,8 +3,8 @@ npm i
 npm version prerelease --force --no-git-tag-version
 echo "Versie "$(./node_modules/.bin/extract-json package.json version)", "$(date '+%d-%m-%Y') > ./Content/Versie.md
 
-# Generate into folder $1 the document $2.pdf titled $3
-# generate <document definition folder> <name of document output without extension>
+# Generate into folder $1 the document $2.pdf, titled $3.
+# generate <document definition folder> <name of document output without extension> <title>
 function generate {
     mkdir -p Generated/$1
     # Cover
@@ -23,13 +23,40 @@ function generate {
     wkhtmltopdf --footer-html DocumentDefinitions/Shared/footer.html --footer-spacing 10 \
         --header-html Generated/$1/header.html --header-spacing 10 \
         --margin-bottom 27 --margin-left 34 --margin-right 34 --margin-top 27 \
-        cover Generated/$1/cover.html toc --xsl-style-sheet DocumentDefinitions/Shared/toc.xsl \
+        cover Generated/$1/cover.html \
+        toc --xsl-style-sheet DocumentDefinitions/Shared/toc.xsl \
         Generated/$1/document.html $2.pdf
+}
+
+# Generate into folder Templates/$1 the template document $2.pdf, titled $3.
+# generate-template <document definition folder> <name of document output without extension> <title>
+function generate-template {
+    mkdir -p Generated/Templates/$1
+    # Cover
+    sed s/{{TITLE}}/$3/g DocumentDefinitions/Templates/Shared/cover.md > Generated/Templates/$1/cover-without-includes.md
+    node node_modules/markdown-include/bin/cli.js ./DocumentDefinitions/Templates/$1/cover.json
+    node_modules/markdown-to-html/bin/markdown Generated/Templates/$1/cover.md \
+        -s /ka/DocumentDefinitions/Shared/cover.css | \
+        PYTHONIOENCODING="UTF-8" python3 post-process-html.py > Generated/Templates/$1/cover.html
+    # Body
+    node node_modules/markdown-include/bin/cli.js ./DocumentDefinitions/Templates/$1/document.json
+    node_modules/markdown-to-html/bin/markdown Generated/Templates/$1/document.md \
+        -s /ka/DocumentDefinitions/Shared/document.css | \
+        PYTHONIOENCODING="UTF-8" python3 post-process-html.py > Generated/Templates/$1/document.html
+    # Create header
+    sed s/{{TITLE}}/$3/g DocumentDefinitions/Shared/header.html > Generated/Templates/$1/header.html
+    # Create pdf
+    wkhtmltopdf --footer-html DocumentDefinitions/Shared/footer.html --footer-spacing 10 \
+        --header-html Generated/Templates/$1/header.html --header-spacing 10 \
+        --margin-bottom 27 --margin-left 34 --margin-right 34 --margin-top 27 \
+        cover Generated/Templates/$1/cover.html \
+        toc --xsl-style-sheet DocumentDefinitions/Shared/toc.xsl \
+        Generated/Templates/$1/document.html $2.pdf
 }
 
 generate Full ICTU-Kwaliteitsaanpak-Full "ICTU Kwaliteitsaanpak Software Realisatie"
 generate Generic ICTU-Kwaliteitsaanpak-Generic "Kwaliteitsaanpak Software Realisatie"
-generate Templates/Kwaliteitsplan Template-Kwaliteitsplan "Kwaliteitsplan"
-generate Templates/NFE Template-Niet-Functionele-Eisen "Niet-Functionele Eisen"
+generate-template Kwaliteitsplan Template-Kwaliteitsplan "Kwaliteitsplan"
+generate-template NFE Template-Niet-Functionele-Eisen "Niet-Functionele Eisen"
 
 python3 create-checklist.py

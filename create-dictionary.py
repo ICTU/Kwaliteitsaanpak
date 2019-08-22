c97b8ada7445737044eddc85bc42f05e5c5b1cc6
Barleduc <lepelrekje@xs4all.nl>
2019-08-22 12:42:20 +0200

    dictionary script

--- create-dictionary.py
+++ create-dictionary.py
@@ -4,7 +4,10 @@ import re
 def create_dictionary():
     with open("Content/Versie.md") as version_file:
         version = version_file.read().strip()
-        print("{{VERSIE}}:{{" + version + "}}\n")
+        print("{{VERSIE}}:{{" + version + "}}")
+    path = pathlib.Path('Content/Maatregelen')
+    for mdir in path.glob('*'):
+        print("{{" + mdir.name + "}}")
 
 if __name__ == "__main__":
     create_dictionary()

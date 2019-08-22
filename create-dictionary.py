d25a5a77416f59ab7428bbba0f8533a0937c9d1a
Barleduc <lepelrekje@xs4all.nl>
2019-08-22 09:07:58 +0200

    dictionary script

--- create-dictionary.py
+++ create-dictionary.py
@@ -4,7 +4,7 @@ import re
 def create_dictionary():
     with open("Content/Versie.md") as version_file:
         version = version_file.read().strip()
-        print ('{{VERSIE}}:{{', version, '}}\n')
+        print("{{VERSIE}}:{{" + version + "}}\n")
 
 if __name__ == "__main__":
     create_dictionary()

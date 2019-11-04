bf8c632e912d7e2eb6cbed2ea9a2654b09b00841
Frank Niessink <frank@niessink.com>
2019-11-04 22:20:04 +0100

    Add dotnet project.

--- create-docs.sh
+++ create-docs.sh
@@ -147,9 +147,7 @@ function generate-template
     generate $TEMPLATE_PATH $2 "$TITLE" "$HEADER" $COVER_MD $DOC_MD $MAATREGEL_DICTIONARY $DOCX_REF
 }
 
-#docker build dotnet-helloword -t helloworlddotnet --no-cache
-
-#docker run -v `pwd`/dotnet-helloword/output:/output helloworlddotnet
+docker-compose run dotnet
 
 python3 create-dictionary.py > $MAATREGEL_DICTIONARY
 python3 create-dictionary.py --link > $MAATREGEL_DICTIONARY_LINKS
